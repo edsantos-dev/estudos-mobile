44b8e184aa2c6b9552511e3b47c1226e1862c420
Eduardo Alan dos Santos <edualansatos@gmail.com>
2026-04-11 15:46:12 -0300

    feat: cria a interface principal juntando o display e o teclado de botões

---
+++ lib/trabalho_3/calculadora.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'widgets/botao.dart';
+import 'widgets/display.dart';
+
+class Calculadora extends StatefulWidget {
+  const Calculadora({super.key});
+
+  @override
+  State<Calculadora> createState() => _CalculadoraState();
+}
+
+class _CalculadoraState extends State<Calculadora> {
+  
+  String _valorDisplay = '0';
+
+  void _botaoPressionado(String texto) {
+    print("Botão $texto foi pressionado!");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculadora Flutter'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Column(
+        children: [
+          Display(valor: _valorDisplay),
+          
+          // 2. O teclado da calculadora
+          Expanded(
+            flex: 3,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Botao(texto: '7', aoClicar: () => _botaoPressionado('7')),
+                      Botao(texto: '8', aoClicar: () => _botaoPressionado('8')),
+                      Botao(texto: '9', aoClicar: () => _botaoPressionado('9')),
+                      Botao(texto: '/', cor: Colors.orange, aoClicar: () => _botaoPressionado('/')),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Botao(texto: '4', aoClicar: () => _botaoPressionado('4')),
+                      Botao(texto: '5', aoClicar: () => _botaoPressionado('5')),
+                      Botao(texto: '6', aoClicar: () => _botaoPressionado('6')),
+                      Botao(texto: 'x', cor: Colors.orange, aoClicar: () => _botaoPressionado('x')),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Botao(texto: '1', aoClicar: () => _botaoPressionado('1')),
+                      Botao(texto: '2', aoClicar: () => _botaoPressionado('2')),
+                      Botao(texto: '3', aoClicar: () => _botaoPressionado('3')),
+                      Botao(texto: '-', cor: Colors.orange, aoClicar: () => _botaoPressionado('-')),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Botao(texto: 'C', cor: Colors.redAccent, aoClicar: () => _botaoPressionado('C')),
+                      Botao(texto: '0', aoClicar: () => _botaoPressionado('0')),
+                      Botao(texto: '=', cor: Colors.green, aoClicar: () => _botaoPressionado('=')),
+                      Botao(texto: '+', cor: Colors.orange, aoClicar: () => _botaoPressionado('+')),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
